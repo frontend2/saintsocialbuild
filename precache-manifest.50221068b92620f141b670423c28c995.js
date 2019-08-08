de001d9e2458050aadd7d08b4e638ad756c03207
Christian Páez <frontend2@haytic.com>
2019-08-08 16:39:09 -0500

    Updates

---
+++ precache-manifest.50221068b92620f141b670423c28c995.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88f5c1d09a04fbd36a27e8c9d2ad9f1",
+    "url": "/saintsocialbuild/index.html"
+  },
+  {
+    "revision": "968b53d2dde3516789d4",
+    "url": "/saintsocialbuild/static/css/2.81cb803e.chunk.css"
+  },
+  {
+    "revision": "11aa367c90e8c4b71fff",
+    "url": "/saintsocialbuild/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "968b53d2dde3516789d4",
+    "url": "/saintsocialbuild/static/js/2.f9728f61.chunk.js"
+  },
+  {
+    "revision": "11aa367c90e8c4b71fff",
+    "url": "/saintsocialbuild/static/js/main.f3dc8a4d.chunk.js"
+  },
+  {
+    "revision": "4e6ba20f9bdaf964da06",
+    "url": "/saintsocialbuild/static/js/runtime~main.c0093636.js"
+  }
+]);
